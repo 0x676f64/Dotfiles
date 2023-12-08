ce3c8009019f0ae04eb668236140d14bb03015b1
Joey Rodriguez <oscardiggz@protonmail.com>
2023-12-08 15:44:24 -0600

    Update packer.lua

--- .config/nvim/lua/0x676f64/packer.lua
+++ .config/nvim/lua/0x676f64/packer.lua
@@ -10,22 +10,34 @@ use {
 	-- or                            , branch = '0.1.x',
 	requires = { {'nvim-lua/plenary.nvim'} }
 }
-
+use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+use("nvim-treesitter/nvim-treesitter-context");
 use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use('nvim-treesitter/nvim-treesitter-context')
 use ('ThePrimeagen/harpoon')
 use('mbbill/undotree')
 use('tpope/vim-fugitive')
+use('itspriddle/vim-themes')
 use ('nvim-lualine/lualine.nvim')
 use ('onsails/lspkind-nvim')
+use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+				vim.cmd('colorscheme rose-pine')
+			end
+		})
 use ('nvim-tree/nvim-web-devicons')
-use ('vim-airline/vim-airline')
 use ('ThePrimeagen/refactoring.nvim')
 use('barrett-ruth/live-server.nvim')
 use 'rcarriga/nvim-notify'
+use 'rktjmp/lush.nvim'
 use "lukas-reineke/indent-blankline.nvim"
-use ('Mofiqul/dracula.nvim')
 use {
 	"windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
