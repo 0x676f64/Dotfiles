fb80abc40f58702f850e967fea818b1adb1343d2
Joey Rodriguez <oscardiggz@protonmail.com>
2023-12-08 15:40:42 -0600

    Create ram.sh

---
+++ .config/i3blocks/scripts/ram.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+total=$(expr $(cat /proc/meminfo | grep MemTotal | head -n 1 | awk '{ print $2 }') / 1024)
+free=$(expr $(cat /proc/meminfo | grep MemFree | head -n 1 | awk '{ print $2 }') / 1024)
+shmem=$(expr $(cat /proc/meminfo | grep Shmem | head -n 1 | awk '{ print $2 }') / 1024)
+buffers=$(expr $(cat /proc/meminfo | grep Buffers | head -n 1 | awk '{ print $2 }') / 1024)
+cached=$(expr $(cat /proc/meminfo | grep Cached | head -n 1 | awk '{ print $2 }') / 1024)
+reclaimable=$(expr $(cat /proc/meminfo | grep SReclaimable | head -n 1 | awk '{ print $2 }') / 1024)
+
+used=$(expr $total + $shmem - $free - $buffers - $cached - $reclaimable)
+swap=$(free -h | tail -n 1 | awk '{ print $3 }')
+
+percent=$(echo "scale=1; 100*$used/$total" | bc)
+printf " %s%%/%'dMb/$swap  \n" $percent $used
